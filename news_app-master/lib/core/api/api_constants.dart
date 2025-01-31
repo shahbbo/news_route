6da7ba699f4e25e2063f0fe6d1c5d84ec4e41508
Mahmoud Shahbo <154024652+shahbbo@users.noreply.github.com>
2025-01-31 19:09:17 +0200

    update

--- news_app-master/lib/core/api/api_constants.dart
+++
@@ -1,4 +0,0 @@
-class ApiConstants {
-  static const String baseUrl = 'newsapi.org';
-  static const String apiKey = 'aa8729ce15ce46c6bb7b1f6042fdcfa8';
-}
--- news_app-master/lib/core/api/end_points.dart
+++
@@ -1,4 +0,0 @@
-class EndPoints {
-  static const String sourceApi = '/v2/top-headlines/sources';
-  static const String newsApi = '/v2/everything';
-}
